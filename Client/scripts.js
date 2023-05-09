5ace6f36e4ef7e631c9c01293dd16ca64de088da
ZunainAliAzam <zunainazam456@gmail.com>
2023-05-09 18:45:31 +0500

    signup and login

---
+++ Client/scripts.js
@@ -0,0 +1,8 @@
+const loginButton = document.getElementById("login-button");
+
+// Add a click event listener to the login button
+loginButton.addEventListener("click", function() {
+  // Redirect to the login page
+  window.location.href = "login.html";
+});
+
